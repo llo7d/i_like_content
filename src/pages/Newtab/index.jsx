f19e401edca871fc669a4cc703bc39de6eb7e1f4
phil@home <philipp.millner@outlook.com>
2024-04-29 17:41:38 +0200

    index.css is now imported

--- src/pages/Newtab/index.jsx
+++ src/pages/Newtab/index.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { createRoot } from 'react-dom/client';
 
 import Newtab from './Newtab';
-import './index.css';
+import './css/index.css';
 
 const container = document.getElementById('app-container');
 const root = createRoot(container); // createRoot(container!) if you use TypeScript
